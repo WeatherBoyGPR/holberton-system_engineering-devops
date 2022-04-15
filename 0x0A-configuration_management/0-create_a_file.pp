b099fa48298583cf0d483e9f603453632885d3e6
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-04-15 22:55:04 +0000

    added 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+# Will create a file containing 'I love puppet' in /tmp directory
+file { '/tmp/school':
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
