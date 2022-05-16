eb7c740284b11d38f0073ec9bbdcc2a237489991
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-05-16 02:30:52 +0000

    added 0-script.sql

---
+++ 0x14-mysql/0-script.sql
@@ -0,0 +1,3 @@
+# Will create user as per task instructions
+CREATE USER 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
