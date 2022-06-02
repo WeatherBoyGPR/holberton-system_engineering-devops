3c2adbf6c234800951e4a16a560f327f9462d5f8
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-06-02 02:13:31 +0000

    added task 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixes file name typo 0x17-Debugging
+
+exec{ 'fix-typo':
+    command => "sed -i \'s/class-wp-locale.phpp/class-wp-locale.php/g\' /var/www/html/wp-settings.php",
+    path    => "/usr/local/bin/:/bin/",
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,3 @@
+# 0x17-web_stack_debugging
+
+0-strace_is_your_friend.pp
